bbff7309fcb07b2c02c348e6913919f48018b52e
Your Name <your@email.com>
2023-05-29 19:42:41 -0600

    update with excercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+average = Store.average(:annual_revenue)
+store_count = Store.where("annual_revenue >= ?", 1000000).count
+
+puts "Total revenue for the entire company: $#{total_revenue}"
+puts "Average annual revenue for all stores: $#{average}"
+puts "Number of stores generating $1M or more in annual sales: #{store_count}"
